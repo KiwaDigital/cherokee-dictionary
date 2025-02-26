9b8148ef45bc6683523be84f1ca1e821f75a25d7
Jordan Koziol-Repia <39073971+KiwaDigital@users.noreply.github.com>
2025-02-26 15:13:01 +1300

    Add files via upload

--- script.js
+++ script.js
@@ -35,7 +35,18 @@ function searchWord() {
                     row["English search 2"],
                     row["English search 3"],
                     row["English search 4"],
-                    row["Syllabary"]
+                    row["Syllabary"],
+                    row["Entry 1A"],
+                    row["Entry 1B"],
+                    row["Entry 1C"],
+                    row["Entry 1D"],
+                    row["Entry 2A"]
+                    row["Entry 2B"],
+                    row["Entry 3A"],
+                    row["Entry 3B"],
+                    row["Tone and length 1"],
+                    row["Segments"],
+                    row["Tone and length 2"]
                 ];
 
                 return columnsToSearch.some(column => 
